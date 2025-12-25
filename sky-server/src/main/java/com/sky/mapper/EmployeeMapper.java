5f61070177083fe92f7191c5e728ee1791ebe16f
Michael Kang <3362690944@qq.com>
2025-12-25 11:56:30 +0800

    员工分页查询代码完善

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,8 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -19,5 +21,6 @@ public interface EmployeeMapper {
     //插入员工数据
     @Insert("insert into employee (username, name, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) values (#{username}, #{name}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Employee employee);
-
+    //分页查询员工
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
