a57970a6de6f6f576b292c47e2852f3108541212
Michael Kang <3362690944@qq.com>
2026-01-12 19:58:59 +0800

    订单支付功能完整代码开发

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -6,8 +6,12 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
+
     @Select("select * from user where openid = #{openid}")
     User getByOpenid(String openid);
 
     void insert(User user);
+
+    @Select("select * from user where id = #{userId}")
+    User getById(Long userId);
 }
